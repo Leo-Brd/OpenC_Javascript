abca8fd2f81da48726ba0f393075597807e1f25b
Leo-Brd <leobordet.pro@gmail.com>
2024-09-20 16:37:29 +0200

    finish P1C3

---
+++ P1/C3/main.js
@@ -0,0 +1,12 @@
+
+let totalLivres = 500;
+
+totalLivres += 50;
+
+totalLivres -= 10;
+
+totalLivres += 5;
+
+let affichageTotalLivres = "Notre bibliothèque possède " + totalLivres + " livres."
+
+console.log(affichageTotalLivres)
