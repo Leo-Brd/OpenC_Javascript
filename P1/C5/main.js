0d69894e73d173ff0252a1f7a6c9c08e32858ebf
Leo-Brd <leobordet.pro@gmail.com>
2024-09-20 21:46:23 +0200

    finish P1C5

---
+++ P1/C5/main.js
@@ -0,0 +1,11 @@
+
+const playlist = ["Me and your mama", "Million dollar baby", "Dancing in the flames"]
+
+let totalMorceaux = playlist.length
+
+playlist.push("Miss you")
+playlist.push("Breathe")
+
+playlist.pop()
+
+console.log(playlist)
