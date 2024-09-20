bf2a39dece61d5127fac87ad065dda71b2c1495d
Leo-Brd <leobordet.pro@gmail.com>
2024-09-20 17:10:29 +0200

    finish P1C4

---
+++ P1/C4/main.js
@@ -0,0 +1,12 @@
+
+const ticket = {
+    nomFilm : "Batman",
+    prix : 10,
+    numeroSalle : 15
+};
+
+let nom = "leo";
+
+let texteAffichage = `Bonjour ${nom}, votre film ${ticket.nomFilm} est en salle ${ticket.numeroSalle}`;
+
+console.log(texteAffichage);
